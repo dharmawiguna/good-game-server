98aa44e49d69a83895348a0838320e2c36ad6ef4
dharmawiguna <dharmawiguna982@gmail.com>
2023-09-06 18:45:28 +0800

    api category

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,5 +1,6 @@
 const Player = require("./model");
 const Voucher = require("../voucher/model");
+const Category = require("../category/model");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -28,6 +29,17 @@ module.exports = {
       }
 
       res.status(200).json({ data: voucher });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || "Internal Server Error" });
+    }
+  },
+
+  category: async (req, res) => {
+    try {
+      const category = await Category.find();
+      res.status(200).json({ data: category });
     } catch (error) {
       res
         .status(500)
