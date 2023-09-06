db6041a87f1f4c7dac8f9333d37efac23ef5a772
dharmawiguna <dharmawiguna982@gmail.com>
2023-09-06 17:49:27 +0800

    api detail page

--- app/player/controller.js
+++ app/player/controller.js
@@ -15,4 +15,23 @@ module.exports = {
         .json({ message: error.message || "Internal Server Error" });
     }
   },
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const voucher = await Voucher.findOne({ _id: id })
+        .populate("user", "_id name phoneNumber")
+        .populate("nominal")
+        .populate("category");
+
+      if (!voucher) {
+        return res.status(404).json({ message: "Voucher not found!" });
+      }
+
+      res.status(200).json({ data: voucher });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || "Internal Server Error" });
+    }
+  },
 };
