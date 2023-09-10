43cea7e51f0623508764de96b39fc87109d15747
dharmawiguna <dharmawiguna982@gmail.com>
2023-09-10 12:50:43 +0800

    api signin

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -2,6 +2,8 @@ const Player = require("../player/model");
 const path = require("path");
 const fs = require("fs");
 const config = require("../../config");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   signUp: async (req, res) => {
@@ -66,5 +68,51 @@ module.exports = {
       }
       next(error);
     }
+  },
+
+  signIn: (req, res, next) => {
+    const { email, password } = req.body;
+
+    Player.findOne({ email: email })
+      .then((player) => {
+        if (player) {
+          const checkPassword = bcrypt.compareSync(password, player.password);
+
+          if (checkPassword) {
+            const token = jwt.sign(
+              {
+                player: {
+                  id: player.id,
+                  username: player.username,
+                  email: player.email,
+                  name: player.name,
+                  phoneNumber: player.phoneNumber,
+                  avatar: player.avatar,
+                },
+              },
+              config.jwtKey
+            );
+
+            res.status(200).json({
+              data: { token },
+            });
+          } else {
+            res.status(403).json({
+              message: "Password is Wrong!",
+            });
+          }
+        } else {
+          res.status(403).json({
+            message: "Email not registered!",
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: err.message || `Internal Server Error`,
+        });
+
+        next();
+      });
   },
 };
--- config/index.js
+++ config/index.js
@@ -6,5 +6,6 @@ dotenv.config();
 module.exports = {
   rootPath: path.resolve(__dirname, ".."),
   servicesName: process.env.SERVICE_NAME,
+  jwtKey: process.env.JWT_SECRET,
   urlDb: process.env.MONGO_URL,
 };
